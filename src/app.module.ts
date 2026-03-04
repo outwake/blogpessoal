a43a094d4b873ec2edef79df21e360f894b85e6e
Lary <larissa.mendy@gmail.com>
2026-03-04 20:33:57 -0300

    :sparkles: Adição da Entidade Tema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostagemModule } from './postagem/postagem.module';
 import { Postagem } from './postagem/entities/postagem.entity';
+import { TemaModule } from './tema/tema.module';
+import { Tema } from './tema/entities/tema.entity';
 
 
 @Module({
@@ -13,11 +15,12 @@ import { Postagem } from './postagem/entities/postagem.entity';
     username: 'root',
     password: 'root',
     database: 'db_blogpessoal',
-    entities: [Postagem],
+    entities: [Postagem, Tema],
     //Synchronize é para sincronizar as funções, em produção deixamos a opção em false
     synchronize: true,
+    logging: true,
   }),
-  PostagemModule
+  PostagemModule, TemaModule
   ],
   controllers: [],
   providers: [],
