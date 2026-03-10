7bf2aec3c803ce07d7ba42b618e596bea51b39f8
Lary <larissa.mendy@gmail.com>
2026-03-10 14:23:20 -0300

    👌 IMPROVE:  Incrementação de security

--- src/tema/tema.module.ts
+++ src/tema/tema.module.ts
@@ -10,7 +10,7 @@ import { TemaService } from "./services/tema.service";
     imports: [TypeOrmModule.forFeature([Tema])],
     controllers:[TemaController],
     providers:[TemaService],
-    exports:[]
+    exports:[TemaService]
 })
 
 export class TemaModule{}
