39696f9cac9ca14466f889f48c8291abf577d938
Lary <larissa.mendy@gmail.com>
2026-03-16 13:13:51 -0300

    👌 IMPROVE: Modificação na classe Tema

--- src/tema/entities/tema.entity.ts
+++ src/tema/entities/tema.entity.ts
@@ -2,23 +2,27 @@ import { Transform, TransformFnParams } from "class-transformer";
 import { IsNotEmpty } from "class-validator";
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Postagem } from "../../postagem/entities/postagem.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 
 @Entity({name: "tb_temas"})
 
 export class Tema{
     //CRIAÇÃO DO ID
+    @ApiProperty() 
     @PrimaryGeneratedColumn()
     id: number;
 
 
     //CRIAÇÃO DA DESCRIÇÃO DO TEMA
+    @ApiProperty() 
     @Transform(({value}: TransformFnParams) => value?.trim())
     @IsNotEmpty()
     @Column({length: 100, nullable: false})
     descricao: string;
 
     //Relacionamento com postagem
+    @ApiProperty()
     @OneToMany(() => Postagem, (postagem)=> postagem.tema)
     postagem: Postagem[];
 }
