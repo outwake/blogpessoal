5f787c10198d4b196319bb2072f92010c8e4931e
Lary <larissa.mendy@gmail.com>
2026-03-16 13:11:22 -0300

    ✨ New Feature: Modificação APP

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,25 +7,27 @@ import { Tema } from './tema/entities/tema.entity';
 import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { Usuario } from './usuario/entities/usuario.entity';
+import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';
 
 
 @Module({
   //Criação da conexão com o banco de dados
-  imports: [ TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'db_blogpessoal',
-    entities: [Postagem, Tema, Usuario],
-    //Synchronize é para sincronizar as funções, em produção deixamos a opção em false
+  imports: [ ConfigModule.forRoot(),
+  TypeOrmModule.forRootAsync({
+	useClass: ProdService,
+  imports: [ConfigModule],
+  }),
+  
+  TypeOrmModule.forRoot({
+  //Synchronize é para sincronizar as funções, em produção deixamos a opção em false
     synchronize: true,
     logging: true,
   }),
   PostagemModule, TemaModule, AuthModule, UsuarioModule
   ],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
