b2e8a42c6b6d191dbfb404f57ba92d2d03df8b36
Lary <larissa.mendy@gmail.com>
2026-03-16 13:30:45 -0300

    🐛 FIX: Exclusao do syncronize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostagemModule } from './postagem/postagem.module';
-import { Postagem } from './postagem/entities/postagem.entity';
 import { TemaModule } from './tema/tema.module';
-import { Tema } from './tema/entities/tema.entity';
 import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './usuario/usuario.module';
-import { Usuario } from './usuario/entities/usuario.entity';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { ProdService } from './data/services/prod.service';
@@ -18,12 +15,6 @@ import { ProdService } from './data/services/prod.service';
   TypeOrmModule.forRootAsync({
 	useClass: ProdService,
   imports: [ConfigModule],
-  }),
-  
-  TypeOrmModule.forRoot({
-  //Synchronize é para sincronizar as funções, em produção deixamos a opção em false
-    synchronize: true,
-    logging: true,
   }),
   PostagemModule, TemaModule, AuthModule, UsuarioModule
   ],
